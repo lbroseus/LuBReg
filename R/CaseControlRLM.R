5ccf9f8a2fe8a6b949627a2f303b8fa9094c514c
Lucile <lucile@san-g-e12-177.u-ga.fr>
2021-06-21 09:17:13 +0200

    Update

--- R/CaseControlRLM.R
+++ R/CaseControlRLM.R
@@ -1,23 +1,25 @@
 ################################################################################
-# Using limma/lmFit to perform EWAS
-# Continuous phenotype/exposure
+# Functions for fitting multivariate robust linear models (RLM) 
+# And significance testing
+# With binary phenotype (eg: case/control) and paired design
+# DNAme ~ Status + Confounders
 ################################################################################
 
 ################################################################################
-#' Run linear regression using limma: CpG ~ Exposure + Confounders
-#' Warning: does not allow missing data in confounders/exposures
+#' Run differential analysis using limma: CpG ~ Treatment + Confounders
+#' Warning: does not allow missing data in confounders
 #' 
-#'
 #' @param meth_data A numeric matrix containing methylation data (CpGs as rows, samples as columns)
 #' @param covariates A data.frame containing all variables needed for regression (exposures and confounders)
-#' @param exposure A character string with the exposure name (must be a column in covariates)
+#' @param treatment A character string with the status name (must be a column in covariates)
 #' @param clinical_confounders A character vector defining clinical confounders the regression will be adjusted for (must be columns in covariates)
 #' @param technical_confounders A character vector defining technical confounders the regression will be adjusted for (must be columns in covariates)
 #' @param transformToMvalue Boolean. Whether to transform CpG values into M-values. Default: FALSE.
+#' @param paired Boolean. Whether samples are paired. Default: FALSE.
+#' @param pair.lab String. Column name were to find pairing. Default: "pair".
 #' @param robust Boolean. Whether to perform robust linear regression. Default: FALSE.
 #' @param maxit The number of iterations for each robust regression.
 #' @param verbose Boolean. Whether to output messages. Default: TRUE.
-#' @param id.lab String. Column name were to find sample ids. Default: "id".
 #'
 #' @import limma
 #' @import magrittr
@@ -25,42 +27,26 @@
 #' @importFrom stats as.formula model.matrix
 #'
 #' @return An object of class MArrayLM as output by the limma::eBayes() function.
-
-runLimma <- function(meth_data, 
-                     covariates,
-                     exposure,
-                     clinical_confounders = 0,
-                     technical_confounders = 0,
-                     transformToMvalue = FALSE,
-                     robust = FALSE,
-                     verbose = TRUE,
-                     id.lab = "id"){
+#' 
+casectrlAnalysis <- function(meth_data,
+                             covariates,
+                             treatment,
+                             clinical_confounders = 0,
+                             technical_confounders = 0,
+                             transformToMvalue = FALSE,
+                             paired = FALSE,
+                             pair.lab = "pair",
+                             robust = FALSE,
+                             verbose = TRUE){
   
-  if(!(id.lab %in% colnames(covariates))) stop(paste("Input covariate matrix must have a column labelled", id.lab, ".\n"))
-  
-  if( !identical(covariates[,id.lab], colnames(meth_data)) ) stop("Covariates and methylation data do not match \n")
-  
-  if(!(exposure %in% colnames(covariates))) stop("Specified exposure not in input covariate matrix. \n")
-  
-  covariates[,exposure] <- as.numeric(covariates[,exposure])
-  
-  nIndiv <- nrow(covariates)
-  
-  covariates <- na.omit(covariates[, unique(c(id.lab, exposure, clinical_confounders, technical_confounders))])
-  
-  if( nrow(covariates) != nIndiv ){
-    warning(paste(nIndiv-nrow(covariates), 
-                  "individual(s) with missing covariate data had to be removed from the analysis."))
-    meth_data <- meth_data[,intersect(colnames(meth_data), covariates[,id.lab])]
-  }
   
   # Design specification
   formula <-
-    stats::as.formula(paste("~", exposure, "+",
+    stats::as.formula(paste("~", treatment, "+",
                             paste(clinical_confounders, collapse = " + "), "+",
                             paste(technical_confounders, collapse = " + ")
-                            )
-                      )
+    )
+    )
   
   if( verbose ) cat("[LIMMA] Linear model to be fitted: \n"); print(formula)
   
@@ -73,16 +59,18 @@ runLimma <- function(meth_data,
     if( verbose ) cat("OK.\n")
   }
   
-  # Fit linear models
+  if( paired ) cor <- limma::duplicateCorrelation(meth_data, 
+                                     design = design, 
+                                     block = covariates[,pair.lab])
+  
   if( verbose ) cat("[LIMMA] Fitting linear models...")
-  fit <- limma::lmFit(object = meth_data, 
+  fit <- limma::lmFit(object = meth_data,
                       design = design,
-                      method = ifelse(robust, "robust", "ls"), 
-                      maxit = maxit)
+                      block = ifelse(paired, covariates[,pair.lab], NULL),
+                      correlation = ifelse(paired, cor$consensus, NULL))
   # Apply eBayes
-  fit <- limma::eBayes(fit)
+  fit <- eBayes(fit)
   if( verbose ) cat("OK.\n")
   
   return( fit )
-
 }
